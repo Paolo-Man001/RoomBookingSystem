7c96ee5284e37e551f521a2f8cb2eee4728822d1
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-03 13:34:06 +1300

    Add basic routing. declare routes in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {RoomsComponent} from './admin/rooms/rooms.component';
+import {UsersComponent} from './admin/users/users.component';
+import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+/** Routing */
+const routes: Routes = [
+  {path: '', component: CalendarComponent},
+  {path: 'admin/users', component: UsersComponent},
+  {path: 'admin/rooms', component: RoomsComponent},
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)    // NEED to declare for our routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
