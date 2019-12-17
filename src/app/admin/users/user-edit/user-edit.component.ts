ba6d5529275c90f6d160f8ffb6899b1c96a9931c
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-17 15:21:03 +1300

    Implement CustomValidation Rules. Implement rules on password match and validaiton for add-user. display appropriate message and enable/disable save button.

--- src/app/admin/users/user-edit/user-edit.component.ts
+++ src/app/admin/users/user-edit/user-edit.component.ts
@@ -17,7 +17,9 @@ export class UserEditComponent implements OnInit {
   message: string;
   password: string;
   password2: string;
-
+  nameIsValid = false;
+  passwordsMatch = false;
+  passwordsAreValid = false;
 
   constructor(private dataService: DataService,
               private router: Router) {
@@ -25,10 +27,13 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit() {
     this.formUser = Object.assign({}, this.user)
+    this.checkIfNameIsValid();
+    this.checkIfPasswordsAreValid();
   }
 
 
-  // METHODS:
+  /** METHODS: */
+  // onSubmit
   onSubmit() {
     if (this.formUser.id == null) {
       this.dataService.addUser(this.formUser, this.password)
@@ -49,5 +54,34 @@ export class UserEditComponent implements OnInit {
         });
     }
   } // End onSubmit()
+
+
+  // checkIfNameIsValid
+  checkIfNameIsValid() {
+    if (this.formUser.name) {   // check if there is a name/character
+      this.nameIsValid = this.formUser.name.trim().length > 0;  // check if name is blank or not
+    } else {
+      this.nameIsValid = false;
+    }
+  }
+
+
+  // checkIfPasswordsAreValid
+  checkIfPasswordsAreValid() {
+    if (this.formUser.id != null) {
+
+      this.passwordsAreValid = true;
+      this.passwordsMatch = true;
+
+    } else {
+
+      this.passwordsMatch = this.password === this.password2;
+      if (this.password) {
+        this.passwordsAreValid = this.password.trim().length > 0;
+      } else {
+        this.passwordsAreValid = false;
+      }
+    }
+  }
 
 }
